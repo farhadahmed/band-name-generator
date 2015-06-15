318afe806cd98be331eb472605342ed980c47ad3
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-06-15 02:20:14 -0700

    Add event listeners to post verbs and nouns to /verb and /noun

--- app/script.js
+++ app/script.js
@@ -7,7 +7,20 @@ $(function() {
     $.post('/adjective', {word: userAdjective}, function(response) { //this is json object and a callback function
       var confirm = response.message + " We saved: <em>" + response.confirm + "</em>";
       $('#adjective-res').html(confirm);
-    })
+    });
+
+    var userVerb = $('#user-verb').val();
+    $.post('/verb', {word: userVerb}, function(response) {
+      var confirm = response.message + " We saved: <em>" + response.confirm + "</em>";
+      $('#verb-res').html(confirm);
+    });
+
+    var userNoun = $('#user-noun').val();
+    $.post('/noun', {word: userNoun}, function(response) {
+      var confirm = response.message + " We saved: <em>" + response.confirm + "</em>";
+      $('#noun-res').html(confirm);
+    });
+
   })
 
   $("button").click(function() {
