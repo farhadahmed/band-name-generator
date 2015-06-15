fa1844a005070b56c7ea2fdbe4ed48978c0379bb
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-06-14 21:34:06 -0700

    Add event handler for form submission which uses post to send data to server

--- app/script.js
+++ app/script.js
@@ -1,19 +1,26 @@
 'use strict';
 
 $(function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var userAdjective = $('#user-adjective').val();
+    $.post('/adjective', {word: userAdjective}, function() { //this is json object and a callback function
+      // Get something back, do something with it!
+    })
+  })
 
   $("button").click(function() {
-    $.get('adjective', function(response) {
+    $.get('/adjective', function(response) {
       var adjective = response.word;
       $("#adjective").text(adjective);
     });
 
-    $.get('verb', function(response) {
+    $.get('/verb', function(response) {
       var verb = response.word;
       $("#verb").text(verb);
     });
 
-    $.get('noun', function(response) {
+    $.get('/noun', function(response) {
       var noun = response.word;
       $("#noun").text(noun);
     });
