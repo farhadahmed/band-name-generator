7862405e9eb77a70298c0c29b6d3579523e2ce05
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-06-14 21:38:13 -0700

    Add dependency for body-parser in server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/app/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var Adjective = function() {
   this.sleepy = true;
