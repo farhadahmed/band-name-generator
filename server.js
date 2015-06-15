546e888ce8fce13a586d6e97ff2f27b936ba3e73
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-06-15 15:15:26 -0700

    Refactor code to use postWord function

--- server.js
+++ server.js
@@ -68,19 +68,22 @@ var noun = new Noun();
 app.post('/adjective', function(req, res) {
   console.log(req.body.word);
   adjective[req.body.word] = true;
-  res.json({message: "Ya did it!", confirm: req.body.word}) //sending back a json object that has two parts to it.
+  res.json(postWord(req.body.adjective, adjective));
+  //res.json({message: "Ya did it!", confirm: req.body.word}) //sending back a json object that has two parts to it.
 })
 
 app.post('/verb', function(req, res) {
   console.log(req.body.word);
   verb[req.body.word] = true;
-  res.json({message: "You added a verb!", confirm: req.body.word}) //sending back a json object that has two parts to it.
+  res.json(postWord(req.body.verb, verb));
+  //res.json({message: "You added a verb!", confirm: req.body.word}) //sending back a json object that has two parts to it.
 })
 
 app.post('/noun', function(req, res) {
   console.log(req.body.word);
   noun[req.body.word] = true;
-  res.json({message: "You added a noun!", confirm: req.body.word}) //sending back a json object that has two parts to it.
+  res.json(postWord(req.body.noun, noun));
+  //res.json({message: "You added a noun!", confirm: req.body.word}) //sending back a json object that has two parts to it.
 })
 
 app.get('/adjective', function(req, res) {
